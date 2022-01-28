ca8a794b2d7b881d2ac159591e086b5a737d8b09
Chris McClarin <chrismcclarin@Chriss-Air.attlocal.net>
2022-01-27 18:04:00 -0600

    frontend done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from "react-router-dom"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+render(
+  <Router>
+      <App />
+  </Router>,
   document.getElementById('root')
 );
 
